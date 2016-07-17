0984071a5759c850c2c5c2d4357c11be5c81d058
lsperber <lsperber@etsy.com>
2016-07-17 15:21:28 -0400

    makes clicking on nav items scroll to the appropriate section

--- savethedate.js
+++ savethedate.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     var CONSTANTS = {
         SPACE_KEY_CODE: 32,
+        NAV_PADDING: 5,
         PHRASES: [
             "convenience = MAX_INT",
             "romance = MAX_INT",
@@ -18,8 +19,10 @@ $(document).ready(function () {
         icon = videoButton.find('i'),
         videoElement = $('[data-ui="video"]')[0],
         footerElement = $('[data-ui="footer-text"'),
+        navLinks = $('[data-ui="nav-item'),
         navElement = $('[data-ui="nav"]'),
         topOfNav = navElement[0].offsetTop,
+        navHeight = navElement[0].offsetHeight,
         windowElement = $(window);
 
     var toggleVideoStatus = function () {
@@ -63,6 +66,16 @@ $(document).ready(function () {
 
     windowElement.on('scroll', function () {
         resetNavClass();
+    });
+
+    navLinks.on('click', function (event) {
+        var item = event.target.dataset.navItem,
+            targetHeaderElement = $('[data-header-item="' + item + '"]')[0],
+            targetY = targetHeaderElement.offsetTop - targetHeaderElement.offsetHeight - navHeight - CONSTANTS.NAV_PADDING;
+
+        $('html, body').animate({
+          scrollTop: targetY
+        }, 1000);
     });
 
     changeFooterText();
